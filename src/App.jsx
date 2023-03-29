56ed322d29610a693bcfb249bdd65f5e8494fbfc
Mask02 <shoaibakbarm@gmail.com>
2023-03-29 13:16:38 +0500

    first commit project intializ

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import { BrowserRouter as Router,Routes, Route } from 'react-router-dom'
+import Header from './components/Header'
+import './styles/App.css'
+
+function App() {
+
+  return (
+    <>
+    <Header/>
+    <Router>
+      <Routes>
+        
+      </Routes>
+    </Router>
+    </>
+  )
+}
+
+export default App
